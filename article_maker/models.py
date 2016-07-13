4b5c0ac77bda54e2a03b88a2d0f2bcdbea0b8ff4
Triampol <alexandra.lesliex.triampol@intel.com>
2016-07-13 15:42:59 -0500

    initial commit

---
+++ article_maker/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+# Specifications on how the database should be built.
---
+++ article_maker/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+"""
+	Test Case: to see if the name includes numbers
+	Test Case: numbers for letters
+	Test Case: not a real name
+	Test Case: blank
+	Test Case: punctuation
+"""
